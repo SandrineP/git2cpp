8fbf6c16c56fe54b8b869ee3d3e2c511c8da90ad
Ian Thomas <ianthomas23@gmail.com>
2025-07-31 12:04:33 +0100

    Fix printing of help and version info - attempt 2 (#30)

--- src/main.cpp
+++ src/main.cpp
@@ -42,8 +42,7 @@ int main(int argc, char** argv)
         {
             std::cout << "git2cpp version " << GIT2CPP_VERSION_STRING << " (libgit2 " << LIBGIT2_VERSION << ")" << std::endl;
         }
-
-        if (app.get_subcommands().size() == 0 && !version)
+        else if (app.get_subcommands().size() == 0)
         {
             std::cout << app.help() << std::endl;
         }
